f8293b00d1a0e23ee0da8680bd16974430e2e0fc
Steven North <stevenrino123@gmail.com>
2025-08-05 21:44:19 -0500

    added other maps

--- src/index.js
+++ src/index.js
@@ -12,13 +12,26 @@ class Pacman extends Phaser.Scene {
     }
 
     preload(){
+<<<<<<< Updated upstream
         this.load.image("Tileset generic", "assets/tiles/Tileset generic.png");
         this.load.tilemapTiledJSON("map", "assets/maps/pacman-generic-map.json");
+=======
+        //this.load.image("Tileset generic", "assets/levels/1/Tileset generic.png");
+        //this.load.tilemapTiledJSON("map", "assets/levels/1/pacman-generic-map.json");
+        this.load.image("Tileset ice & desert", "assets/levels/2/Tileset ice & desert.png");
+        this.load.tilemapTiledJSON("map", "assets/levels/2/pacman-ice-map.json");
+        
+        // Load ghost images
+        this.load.image("red_ghost", "assets/images/red_ghost.png");
+        this.load.image("pink_ghost", "assets/images/pink_ghost.png");
+        this.load.image("blue_ghost", "assets/images/blue_ghost.png");
+        this.load.image("green_ghost", "assets/images/green_ghost.png");
+>>>>>>> Stashed changes
     }
 
     create(){
         this.map = this.make.tilemap({ key: "map" });
-        const tileset = this.map.addTilesetImage("Tileset generic");
+        const tileset = this.map.addTilesetImage("Tileset ice & desert");
         const layer = this.map.createLayer("Tile Layer 1", tileset);
         layer.setCollisionByExclusion(-1, true);
 
